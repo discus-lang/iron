536308c1ad45393f985d07f2824682a1ee2bf4fe
Ben Lippmeier <benl@ouroborus.net>
2012-12-27 14:37:27 +1100

    F2Effect: add missing top-level value module
    Ignore-this: 19290e7d8a8c9c7be807693c0d1df7d1
    
    darcs-hash:20121227033727-08f32-95dd5dfcdf927d128ffbfff43bd0a738987919d3

---
+++ devel/Iron/Language/SystemF2Effect/Va.v
@@ -0,0 +1,7 @@
+
+Require Export Iron.Language.SystemF2Effect.VaExpBase.
+Require Export Iron.Language.SystemF2Effect.VaExpLift.
+Require Export Iron.Language.SystemF2Effect.VaExpSubst.
+Require Export Iron.Language.SystemF2Effect.VaExpWfX.
+
+
