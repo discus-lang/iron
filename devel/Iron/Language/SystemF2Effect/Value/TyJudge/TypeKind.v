966b47dc7201cc00aae3a23262feeeae7b2315cb
Ben Lippmeier <benl@ouroborus.net>
2013-01-02 00:04:52 +1100

    SystemF2Effect: add effect kind to typex_kind
    Ignore-this: 4714c3cb20708e744e955b5c93ccd719
    
    darcs-hash:20130101130452-08f32-ec2ee448a259de43abb18e7082ae95376542f12f

--- devel/Iron/Language/SystemF2Effect/Value/TyJudge/TypeKind.v
+++ devel/Iron/Language/SystemF2Effect/Value/TyJudge/TypeKind.v
@@ -3,25 +3,25 @@ Require Import Iron.Language.SystemF2Effect.Value.TyJudge.
 Require Import Iron.Language.SystemF2Effect.Theory.SubstTypeType.
 
 
-Lemma typex_kind_type
+Lemma typex_kind_type_effect
  :  forall ke te se sp v t e
  ,  TYPEX  ke te se sp v t e
- -> KIND   ke t KData.
+ -> (KIND ke t KData /\ KIND ke e KEffect).
 Proof.
  intros. gen ke te se sp t e. 
  induction v using exp_mutind with
   (PV := fun v => forall ke te se sp t
       ,  TYPEV  ke te se sp v t
       -> KIND   ke t KData);
-   rip; inverts_type; eauto 3.
+   intros; inverts_type.
 
- Case "XLoc".
+ Case "VLoc".
   admit.                                 (* need to know types in se are well kinded *)
   admit.
 
- Case "XLam".
+ Case "VLam".
   unfold tFun.
-  lets D: IHv H8.
+  lets D: IHv H8. rip.
    eapply KIApp.
     unfold appkind. burn.
     eapply KIApp.
@@ -29,9 +29,10 @@ Proof.
     eapply KIApp.
     unfold appkind. burn.
     eapply KICon. simpl. eauto.
-  auto.
-  admit.                                 (* need e2 effect *)
-  auto.
+  auto. auto. auto.
+
+ Case "VLAM".
+  spec IHv H7. rip.
 
  Case "XConst".
   destruct c; simpl.
@@ -39,30 +40,52 @@ Proof.
   unfold tNat;  burn.
   unfold tBool; burn.
 
+ Case "XVar".
+  burn.
+
  Case "XApp".
-  eapply IHv in H6.
-  eapply IHv0 in H9.
-   unfold tFun in H6.
-   inverts_kind.
-   simpl in H4. inverts H4.
-   auto.
+  lets D1: IHv1 H10.
+  lets D2: IHv2 H11.
+  rip.
+
+ Case "XLet".
+  unfold tFun in *.
+  lets D1: IHv  H6.
+  inverts D1.
+  inverts H3.
+  inverts H7.
+  inverts H8.
+  simpl in *.
+  inverts H4. eauto.
 
  Case "XAPP".
   eapply IHv in H6.
   inverts_kind.
+  rip.
   eapply subst_type_type; eauto.
  
  Case "XNew".
   eapply IHv in H7.
+  rip.
   admit.                               (* go via lemma about lowerTT_substTT *)
+  admit.
+
+ Case "XUse".
+  eapply IHv in H8.
+  rip.
 
  Case "XAlloc". 
-  unfold tRef.
+  unfold tRef. rip.
   eapply KIApp. unfold appkind. burn.
   eapply KIApp. unfold appkind. burn.
   eapply KICon. simpl. eauto.
   auto.
   spec IHv H9. auto.
+  unfold tAlloc in *.
+  eapply KIApp.
+  unfold appkind; burn.
+  eapply KICon; burn.
+  auto.
 
  Case "XRead".
   spec IHv H9.
@@ -73,12 +96,44 @@ Proof.
   inverts H7.
   simpl in *.
   inverts H3.
+  rip.
+  unfold tRead.
+  eapply KIApp.
+  unfold appkind; burn.
+  eapply KICon; burn.
   auto.
 
  Case "XWrite".
-  unfold tUnit. auto.
+  unfold tUnit. rip. 
+  unfold tWrite. 
+  eapply KIApp.
+   unfold appkind; burn.
+   eapply KICon; burn. auto.
+
+ Case "XSucc".
+  unfold tNat. rip.
 
  Case "XIsZero".
-  unfold tBool. auto.
+  unfold tBool. rip.
+Qed.
+Hint Resolve typex_kind_type_effect.
+
+
+Lemma typex_kind_type
+ :  forall ke te se sp v t e
+ ,  TYPEX  ke te se sp v t e
+ -> KIND   ke t KData.
+Proof. 
+ intros. 
+ lets D: typex_kind_type_effect H. rip.
 Qed.
 
+
+Lemma typex_kind_effect
+ :  forall ke te se sp v t e
+ ,  TYPEX  ke te se sp v t e
+ -> KIND   ke e KEffect.
+Proof. 
+ intros. 
+ lets D: typex_kind_type_effect H. rip.
+Qed.
