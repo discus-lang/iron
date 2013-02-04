b4daf7ee5294b8638ec3bbc43d5c2e0642053812
Ben Lippmeier <benl@ouroborus.net>
2013-02-04 20:55:08 +1100

    SystemF2Effect: split new progress theorem into old module
    Ignore-this: 4a480f2c307cb9c618242a82eff7da79
    
    darcs-hash:20130204095508-08f32-398fadd5ad61da0edc7a3d7e9beafebc88445b48

--- devel/Iron/Language/SystemF2Effect/Step.v
+++ devel/Iron/Language/SystemF2Effect/Step.v
@@ -4,13 +4,5 @@ Require Export Iron.Language.SystemF2Effect.Type.
 Require Export Iron.Language.SystemF2Effect.Value.
 Require Export Iron.Language.SystemF2Effect.Step.Pure.
 Require Export Iron.Language.SystemF2Effect.Step.Frame.
+Require Export Iron.Language.SystemF2Effect.Step.Progress.
 
-
-(* A closed, well typed expression is either a value or can 
-   take a step in the evaluation. *)
-Theorem progress
- :  forall se sp ss x t e
- ,  WfS    se sp ss
- -> TYPEX  nil nil se sp x t e
- -> (exists v, x = XVal v) 
- \/ (exists ss' sp' x', STEPF ss fs x ss' fs' x').
