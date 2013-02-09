37731afdd2391903675420ac6db39c634c197241
Ben Lippmeier <benl@ouroborus.net>
2013-02-09 19:00:13 +1100

    SystemF2Effect, redoing EquivT
    Ignore-this: cc6e9874b717d07eedf81fb1cf2acf04
    
    darcs-hash:20130209080013-08f32-8e53b59b64c9750d6312c443c3e6d1bc56f3438b

--- devel/Iron/SystemF2Effect/Type/EquivT.v
+++ devel/Iron/SystemF2Effect/Type/EquivT.v
@@ -4,20 +4,65 @@ Require Export Iron.SystemF2Effect.Type.Exp.Base.
 
 (* Equivalence for sum types *)
 Inductive EquivT : ty -> ty -> Prop :=
- | EqRefl 
-   : forall t
-   , EquivT t t
+ | EqVar
+   : forall 
 
- | EqSumSym
-   : forall t1 t2
-   , EquivT (TSum t1 t2) (TSum t2 t1)
+
+Hint Constructors EquivT.
+
+
+Lemma Equiv_sum_left
+ : forall t1 t1' t2
+ ,  EquivT t1 t1'
+ -> EquivT (TSum t1 t2) (TSum t1' t2).
+Proof.
+ intros.
+ apply EqSumComm.
+ auto.
+
+Lemma Equiv_assoc
+ :  forall t1 t2 t3
+ ,  EquivT (TSum t1 (TSum t2 t3)) 
+           (TSum (TSum t1 t2) t3).
+Proof.
+ intros.
+ eapply EqSumSym.
+ eauto.
+
+ induction t1.
+  auto.
+  inverts H0.
+ 
+ auto.
+ auto.
+
+
+(*
+ | EqTrans
+   :  forall t1 t2 t3
+   ,  EquivT t1 t2
+   -> EquivT t2 t3
+   -> EquivT t1 t3
+
+ | EqSym 
+   :  forall t1 t2
+   ,  EquivT t1 t2
+   -> EquivT t2 t1
 
  | EqSumIdemp
    : forall t1
    , EquivT t1 (TSum t1 t1)
 
+ | EqSumComm
+   : forall t1 t2
+   , EquivT (TSum t1 t2) (TSum t2 t1)
+
+ | EqSumAssoc
+   : forall t1 t2 t3
+   , EquivT (TSum t1 (TSum t2 t3))
+            (TSum (TSum t1 t2) t3)
+
  | EqSumBot
    : forall t1 k
    , EquivT t1 (TSum t1 (TBot k)).
-
-Hint Constructors EquivT.
+*)
--- devel/Iron/SystemF2Effect/Type/SubsT.v
+++ devel/Iron/SystemF2Effect/Type/SubsT.v
@@ -1,14 +1,12 @@
 
 Require Export Iron.SystemF2Effect.Type.Exp.Base.
-Require Export Iron.SystemF2Effect.Type.EquivT.
 
 
 (* Subsumption for sum types *)
 Inductive SubsT : ty -> ty -> Prop :=
- | SbEquiv
-   :  forall t1 t2
-   ,  EquivT t1 t2
-   -> SubsT  t1 t2 
+ | SbRefl
+   :  forall t1
+   ,  SubsT t1 t1
 
  | SbTrans
    :  forall t1 t2 t3
@@ -19,33 +17,48 @@ Inductive SubsT : ty -> ty -> Prop :=
    :  forall t k
    ,  SubsT t (TBot k)
 
- | SbSumWeak
+ | SbSumBelow
    :  forall t1 t2 t3
    ,  SubsT t1 t2
-   -> SubsT (TSum t1 t3) t2
+   -> SubsT t1 t3
+   -> SubsT t1 (TSum t2 t3)
 
  | SbSumAbove
    :  forall t1 t2 t3
-   ,  SubsT t1 t2 -> SubsT t1 t3
-   -> SubsT t1 (TSum t2 t3)
-
- | SbSumEquiv 
-   :  forall t1 t1' t2 t2'
-   ,  EquivT t1 t1' -> EquivT t2 t2'
-   -> SubsT  t1 t2  -> SubsT  t1' t2'.
+   ,  SubsT t1 t2
+   -> SubsT (TSum t1 t3) t2.
 
 Hint Constructors SubsT.
 
 
-Lemma SubsT_sum
- : forall e1 e1' e2
- , SubsT e1 e1' -> SubsT (TSum e1 e2) (TSum e1' e2).
+Lemma SubsT_sum_flip
+ :  forall t1 t2 t3
+ ,  SubsT t1 (TSum t2 t3)
+ -> SubsT t1 (TSum t3 t2).
+Proof.
+ intros. 
+ eapply SbSumBelow.
+ 
+
+Lemma SubsT_sum_left
+ : forall t1 t1' t2
+ , SubsT  t1 t1'
+-> SubsT (TSum t1 t2) (TSum t1' t2).
 Proof.
  intros.
- eapply SbSumAbove. eauto.
+ eapply SbSumAbove. 
+ eapply SbSumBelow. auto.
+eauto.
  have (SubsT e2 e2).
  eapply SbSumWeak with (t3 := e1) in H0.
  have (EquivT (TSum e2 e1) (TSum e1 e2)).
  eapply SbSumEquiv; eauto.
 Qed.
 Hint Resolve SubsT_sum.
+
+(*
+ | SbSumAbove
+   :  forall t1 t2 t3 t4
+   ,  SubsT t1 t2 -> SubsT t3 t4
+   -> SubsT (TSum t1 t3) (TSum t2 t4)
+*)
