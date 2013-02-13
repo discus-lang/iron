f79293e70fe1940b150ae0a326dea31eab7ffe38
Ben Lippmeier <benl@ouroborus.net>
2013-02-13 14:16:09 +1100

    SystemF2Effect: sort out store well formedness relations
    Ignore-this: 8261fd7f6d38bafe98b222d7546a3ce3
    
    darcs-hash:20130213031609-08f32-e7c187cd236034df66a6427bbce98181bf5e5665

--- devel/Iron/SystemF2Effect/Store/Bind.v
+++ devel/Iron/SystemF2Effect/Store/Bind.v
@@ -1,6 +1,7 @@
 
 Require Export Iron.SystemF2Effect.Value.
 Require Import Iron.SystemF2Effect.Value.TyJudge.WeakStProps.
+Require Import Iron.SystemF2Effect.Value.TyJudge.TypeKind.
 
 
 (* Store bindings are the primitive objects we keep in the store.
@@ -55,5 +56,65 @@ Proof.
  intros. inverts H. eauto.
 Qed.
 Hint Resolve typeb_stprops_snoc.
+
+
+(******************************************************************************)
+(* Well typed store.
+   All bindings are well typed. *)
+Definition STORET (se: stenv) (sp: stprops) (ss: store)
+ := Forall2 (TYPEB nil nil se sp) ss se.
+Hint Unfold STORET.
+
+
+(* Weaken store properties in store typing judgement. *)
+Lemma storet_weak_stprops
+ :  forall se sp ss p
+ ,  STORET se sp ss
+ -> STORET se (sp :> SRegion p) ss.
+Proof.
+ intros.
+ unfold STORET in *.
+ eapply Forall2_impl.
+  intros. eapply typeb_stprops_cons. eauto.
+ auto.
+Qed.
+Hint Resolve storet_weak_stprops.
+
+
+(* Extended store is well typed under extended store environment *)
+Lemma storet_snoc
+ :  forall se sp ss r1 v1 t2
+ ,  TYPEV  nil nil se sp v1 t2
+ -> STORET                                     se  sp                   ss
+ -> STORET (tRef (TCap (TyCapRegion r1)) t2 <: se) sp (StValue r1 v1 <: ss).
+Proof.
+ intros.
+ set (tRef' := tRef (TCap (TyCapRegion r1)) t2).
+
+ assert (TYPEB nil nil (tRef' <: se) sp (StValue r1 v1) tRef').
+  apply TbValue.
+   apply typev_stenv_snoc.
+    subst tRef'.
+    assert (closedT t2).
+     unfold closedT.
+     rrwrite (0 = @length ki nil).
+     apply (kind_wfT nil sp t2 KData).
+
+     (* NOTE: This one has to be done manually to instantiate existentials *)
+     apply (typex_kind_type nil nil se sp (XVal v1) t2 (TBot KEffect)).
+    auto. auto. auto.
+     
+ assert (Forall2 (TYPEB nil nil (tRef' <: se) sp) ss se).
+  lets D: (@Forall2_impl stbind ty) 
+                (TYPEB nil nil se sp) 
+                (TYPEB nil nil (tRef' <: se) sp)
+                ss se H0.
+  intros.
+  apply typeb_stenv_snoc.
+   auto.
+   subst tRef'. eauto.
+  auto. auto.
+Qed.
+Hint Resolve storet_snoc.
 
 
---
+++ devel/Iron/SystemF2Effect/Store/Wf.v
@@ -0,0 +1,108 @@
+
+Require Export Iron.SystemF2Effect.Store.Bind.
+Require Export Iron.SystemF2Effect.Step.Frame.
+
+
+(******************************************************************************)
+(* Store typing models the store.
+   All types in the store typing have a corresponding binding in the store.
+   We don't want entries in the store typing that don't have corresponding
+   bindings in the store. *)
+
+Definition STOREM (se: stenv) (ss: store)
+ := length se = length ss.
+Hint Unfold STOREM.
+
+
+(* Extended store environment models the extended store *)
+Lemma storem_snoc
+ :  forall se ss t stv
+ ,  STOREM se ss
+ -> STOREM (t <: se) (stv <: ss).
+Proof.
+ intros.
+ unfold STOREM.
+ have (length se = length ss). 
+ burn.
+Qed.
+Hint Resolve storem_snoc.
+
+
+(*****************************************************************************)
+(* All region handles on frame stack are in store properties.
+
+   We don't require the other way, forcing all store properties to be in 
+   the frame stack. This is because we need to be able to pop a Use frame and
+   still have any region handles in the expression being well typed, along with
+   dangling references into that region. *)
+
+Definition STOREP  (sp : stprops) (fs : stack)
+ := forall n, In (FUse n) fs -> In (SRegion n) sp.
+
+
+(* Weaken frame stack in store properties. *)
+Lemma storep_cons
+ :  forall sp fs p
+ ,  STOREP sp fs
+ -> STOREP (sp :> SRegion p) (fs :> FUse p).
+Proof.
+ unfold STOREP in *.
+ - intros.
+   have HN: (n = p \/ ~(n = p)).
+   inverts HN.
+   + simpl. auto.
+   + assert (In (FUse n) fs).
+      eapply in_tail.
+      have (FUse n <> FUse p) by congruence.
+      eauto. auto.
+     eapply in_split.
+      left. eapply H. auto.
+Qed.
+Hint Resolve storep_cons.
+
+
+(******************************************************************************)
+(* Well formed store. *)
+Definition WfS  (se: stenv) (sp: stprops)  (ss: store)
+ := Forall closedT se
+ /\ STOREM se    ss
+ /\ STORET se sp ss.
+Hint Unfold WfS.
+
+
+(* Well formed store and frame stack. *)
+Definition WfFS (se : stenv) (sp : stprops) (ss : store) (fs : stack) 
+ := Forall closedT se
+ /\ STOREM se ss
+ /\ STORET se sp ss
+ /\ STOREP sp fs.
+
+
+Lemma wfFS_wfS 
+ :  forall se sp ss fs
+ ,  WfFS   se sp ss fs
+ -> WfS    se sp ss.
+Proof. firstorder. Qed.
+
+
+Lemma wfFS_fuse_sregion
+ :  forall se sp ss fs p
+ ,  WfFS se sp ss fs
+ -> In (FUse p)    fs
+ -> In (SRegion p) sp.
+Proof. firstorder. Qed.
+
+
+(* Weaken frame stack in WfFS *)
+Lemma wffs_sregion_cons
+ :  forall se sp ss fs p
+ ,  WfFS se sp ss fs
+ -> WfFS se (sp :> SRegion p) ss (fs :> FUse p).
+Proof. 
+ intros.
+ unfold WfFS. 
+ inverts H. inverts H1. inverts H2.
+ auto.
+Qed.
+Hint Resolve wffs_sregion_cons.
+
