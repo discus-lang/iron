8f6fcc3967c3fc12e8b3e44f194278d3a05a7db0
Ben Lippmeier <benl@ouroborus.net>
2013-03-20 14:21:37 +1100

    SystemF2Effect: do elem_get_not
    Ignore-this: 887b1535467216311977259028b29412
    
    darcs-hash:20130320032137-08f32-b49528e4067ecf5e1bf1bfea979cbacef0caff31

--- devel/Iron/SystemF2Effect/Store/Prop.v
+++ devel/Iron/SystemF2Effect/Store/Prop.v
@@ -25,7 +25,31 @@ Lemma elem_get_not
  :  forall {A} (p : A -> bool) (xx : list A) 
  ,  not (exists ix x, get ix xx = Some x /\ p x = true)
  -> elem p xx = false.
-Proof. admit. Qed.          (* prob ok, elem_get_not *)
+Proof. 
+ intros.
+ induction xx.
+ - simpl. auto.
+ - remember (p a) as X.
+   destruct X.
+   + simpl. 
+     rewrite <- HeqX.
+     assert (exists ix x, get ix (xx :> a) = Some x /\ p x = true).
+      exists 0. exists a. rip.
+     tauto.
+     
+   + simpl. 
+     rewrite <- HeqX. 
+     eapply IHxx.
+     unfold not. 
+     intros.
+     destruct H0 as [ix].
+     destruct H0 as [x].
+     rip.
+     have (get (S ix) (xx :> a) = Some x).
+     assert (exists ix x, get ix (xx :> a) = Some x /\ p x = true).
+      exists (S ix). exists x. rip.
+     tauto.
+Qed.  
 
 
 (********************************************************************)
