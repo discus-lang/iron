98375b57593276463cd2274e91ddbae4efab5b51
Ben Lippmeier <benl@ouroborus.net>
2013-04-05 23:04:39 +1100

    SystemF2Effect: doing LiveE lemmas
    Ignore-this: ccd3bde60108fd2927f8c27b2dc53219
    
    darcs-hash:20130405120439-08f32-9c9bafdc4441457d84e502ee35eb3474f151b5ec

--- devel/Iron/SystemF2Effect/Store/LiveE.v
+++ devel/Iron/SystemF2Effect/Store/LiveE.v
@@ -3,14 +3,68 @@ Require Export Iron.SystemF2Effect.Step.Frame.
 Require Export Iron.SystemF2Effect.Store.Bind.
 
 
+Fixpoint handleOfEffect (e : ty) : option nat
+ := match e with
+    | TCap (TyCapRegion p) => Some p
+    | _                    => None
+    end.
+
+
 (* All region handles in this effect have corresponding 
    FUse frames in the frame stack. *)
-Definition LiveE (fs : stack) (e : ty)
- := forall p
-   ,  (In (TRead  (TCap (TyCapRegion p))) (flattenT e) -> In (FUse p) fs)
-   /\ (In (TWrite (TCap (TyCapRegion p))) (flattenT e) -> In (FUse p) fs)
-   /\ (In (TAlloc (TCap (TyCapRegion p))) (flattenT e) -> In (FUse p) fs).
-Global Opaque LiveE.
+Definition LiveEs (fs : stack) (es : list ty)
+ := Forall (fun e1 => forall p, handleOfEffect e1 = Some p
+                   -> In (FUse p) fs)
+           es.
+
+
+Definition LiveE  (fs : stack) (e : ty)
+ := LiveEs fs (flattenT e).
+
+
+(*******************************************************************)
+Lemma liveEs_equivTs
+ :  forall  ke sp es1 es2 fs
+ ,  EquivTs ke sp es1 es2 KEffect
+ -> LiveEs  fs es1
+ -> LiveEs  fs es2.
+Proof.
+ intros.
+ inverts H.
+ unfold LiveEs in *.
+ snorm.
+ apply  H4 in H. 
+ eapply H0 in H; eauto.
+Qed.
+
+
+Lemma liveE_equivT
+ :  forall ke sp e1 e2 fs 
+ ,  EquivT ke sp e1 e2 KEffect
+ -> LiveE  fs e1
+ -> LiveE  fs e2.
+Proof.
+ intros.
+ unfold LiveE in *.
+ eapply liveEs_equivTs.
+  eapply equivT_equivTs; eauto.
+  auto.
+Qed.
+
+
+(*******************************************************************)
+Lemma liveEs_subsTs
+ :  forall ke sp es1 es2 fs
+ ,  SubsTs ke sp es1 es2 KEffect
+ -> LiveEs fs es1
+ -> LiveEs fs es2.
+Proof.
+ intros.
+ inverts H.
+ unfold LiveEs in *.
+ snorm. 
+ eapply H0; eauto.
+Qed.
 
 
 Lemma liveE_subsT
@@ -19,17 +73,25 @@ Lemma liveE_subsT
  -> LiveE fs e1 
  -> LiveE fs e2.
 Proof.
- admit.       (* TODO: liveE_subsT *)
+ intros.
+ unfold LiveE in *.
+ eapply liveEs_subsTs.
+  eapply subsT_subsTs in H; eauto.
+  auto.
 Qed.
 Hint Resolve liveE_subsT.
 
 
+(********************************************************************)
 Lemma liveE_sum_above
  :  forall e1 e2 fs
  ,  LiveE  fs e1 -> LiveE fs e2 
  -> LiveE  fs (TSum e1 e2).
 Proof.
- admit.       (* TODO: liveE_sum_above. *)
+ intros.
+ unfold LiveE. simpl.
+ unfold LiveEs.
+ eapply Forall_app; eauto.
 Qed.
 Hint Resolve liveE_sum_above.
 
@@ -39,7 +101,11 @@ Lemma liveE_frame_cons
  ,  LiveE  fs        e
  -> LiveE  (fs :> f) e.
 Proof.
- admit.       (* TODO: liveE_frame_cons *)
+ intros.
+ unfold LiveE in *.
+ unfold LiveEs in *.
+ snorm. 
+ right. eauto.
 Qed.
 Hint Resolve liveE_frame_cons.
 
@@ -49,7 +115,15 @@ Lemma liveE_pop_flet
  ,  LiveE  (fs :> FLet t x) e
  -> LiveE  fs e.
 Proof.
- admit.       (* TODO: liveE_pop_flet *)
+ intros.
+ unfold LiveE in *.
+ unfold LiveEs in *.
+ snorm.
+ spec H x0. rip.
+ spec H p.  rip.
+ inverts H.
+  nope.
+  auto.
 Qed.
 
 
@@ -58,7 +132,11 @@ Lemma liveE_maskOnVarT
  ,  LiveE  fs (maskOnVarT n e)
  -> LiveE  fs e.
 Proof.
- admit.       (* TODO: liveE_maskOnVarT *)
+ intros.
+ unfold LiveE in *.
+ unfold LiveEs in *.
+ snorm.
+  admit.      (* ok, effect is on handle, so not masked. *)
 Qed.
 
 
@@ -69,4 +147,8 @@ Lemma liveE_phase_change
 Proof.
  admit.       (* TODO: liveE_phase_change *)
 Qed.
+
+
+Global Opaque LiveE.
+
 
